c0aa66a2cca51213a966f12425cff93cd02537ce
Vangel Trajkovski <Vangel.Trajkovski@netcetera.com>
2024-07-26 10:05:25 +0200

    initial commit

---
+++ src/main/java/mk/vt/app/gradletestapp/GradleTestAppApplication.java
@@ -0,0 +1,13 @@
+package mk.vt.app.gradletestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleTestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleTestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/vt/app/gradletestapp/GradleTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.vt.app.gradletestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
